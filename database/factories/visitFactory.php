4f1bd5cc114b8820856c6d22b1d48ea7cc852fa4
AlejandroCamero <72436513+AlejandroCamero@users.noreply.github.com>
2020-10-27 13:02:38 +0100

    Update visitFactory.php

--- database/factories/visitFactory.php
+++ database/factories/visitFactory.php
@@ -4,14 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\visit::class, function (Faker $faker) {
     return [
-        'id' => \App\visit::all()->random()->id,
-        'tutor_c_id' =>\App\visit::all()->random()->id,
-        'enterprise_id' =>\App\visit::all()->random()->id,
+        'id' => $faker->randomDigitNotNull,
+        'tutor_c_id' =>$faker->randomDigitNotNull,
+        'enterprise_id' =>$faker->randomDigitNotNull,
         'date' => $faker->dateTime($max = 'now', $timezone = null),
         'kms' => $faker->number_format,
         'accepted' => $faker-> boolean,
         'deleted' => $faker-> boolean
-
-        //Hola 
     ];
 });
