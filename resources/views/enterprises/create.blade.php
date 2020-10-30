eef4dc325c02b6995a002e42907a4a4ff97ef8e7
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2020-10-30 07:58:23 +0100

    c

--- resources/views/enterprises/create.blade.php
+++ resources/views/enterprises/create.blade.php
@@ -1,1 +1,20 @@
-Crear
+<form action="{{ url('/enterprises.store')}}" method="post">
+{{csrf_field()}}
+
+<label for="Titulo">Crear empresa</label>
+<br/>
+<br/>
+<label for="Nombre">Nombre: </label>
+<input type="text">
+<br/>
+<br/>
+<label for="Email">Email: </label>
+<input type="text">
+<br/>
+<br/>
+<label for="Borrado">Borrado: </label>
+<input type="boolean">
+<br/>
+<br/>
+<input type="submit" value="Crear">
+</form>
