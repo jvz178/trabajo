7a04ebcbe23a9f2c686cf58a69383db396aacb65
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2020-11-10 13:23:00 +0100

    c

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Route::resource('cycles', 'CycleController');
 
+Route::resource('enterprises', 'EnterpriseController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
