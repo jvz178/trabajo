984b4154db7e497b61b83d455c5a3d9af7c73f8e
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2020-11-11 09:10:46 +0100

    m

--- resources/views/enterprises/create.blade.php
+++ resources/views/enterprises/create.blade.php
@@ -1,17 +1,5 @@
 <form action="{{ url('/enterprises')}}" method="post">
 {{ csrf_field() }}
 
-<label for="Titulo">Crear empresa</label>
-<br/>
-<br/>
-<label for="name">Nombre: </label>
-<input type="text" name="name" id="name" value="">
-<br/>
-<br/>
-<label for="email">Email: </label>
-<input type="email" name="email" id="email" value="">
-<br/>
-<br/>
-<input type="submit" value="Crear">
-<a href="{{ url('enterprises')}}">Volver</a>
+@include('enterprises.form',['Mode'=>'create'])
 </form>
--- resources/views/enterprises/edit.blade.php
+++ resources/views/enterprises/edit.blade.php
@@ -2,17 +2,5 @@
 {{csrf_field()}}
 {{method_field('PATCH')}}
 
-<label for="Titulo">Crear empresa</label>
-<br/>
-<br/>
-<label for="name">Nombre: </label>
-<input type="text" name="name" id="name" value="{{$enterprise->name}}">
-<br/>
-<br/>
-<label for="email">Email: </label>
-<input type="email" name="email" id="email" value="{{$enterprise->email}}">
-<br/>
-<br/>
-<input type="submit" value="Cambiar">
-<a href="{{ url('enterprises')}}">Volver</a>
+@include('enterprises.form',['Mode'=>'edit'])
 </form>
