b3e7a36c1964af2712f5660121e34507809c952d
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2020-11-11 23:34:12 +0100

    c

--- resources/views/enterprises/create.blade.php
+++ resources/views/enterprises/create.blade.php
@@ -1,5 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
 <form action="{{ url('/enterprises')}}" method="post">
 {{ csrf_field() }}
 
 @include('enterprises.form',['Mode'=>'create'])
 </form>
+</div>
+@endsection
--- resources/views/enterprises/edit.blade.php
+++ resources/views/enterprises/edit.blade.php
@@ -1,6 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
 <form action="{{url('/enterprises/'.$enterprise->id)}}" method="post">
 {{csrf_field()}}
 {{method_field('PATCH')}}
 
 @include('enterprises.form',['Mode'=>'edit'])
 </form>
+</div>
+@endsection
