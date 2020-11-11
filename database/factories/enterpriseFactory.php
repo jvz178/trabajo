1c264bbcefed9351f615ccfab248ea1591c59431
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2020-11-11 09:37:00 +0100

    m

--- database/factories/enterpriseFactory.php
+++ database/factories/enterpriseFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\enterprise::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail
+        'email' => $faker->unique()->safeEmail,
+        'deleted' => $faker->boolean = false,
     ];
 });
