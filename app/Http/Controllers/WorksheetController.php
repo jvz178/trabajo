476c0584736d038a5a8a272f69877f710fd1d277
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2020-11-14 10:52:41 +0100

    c

--- app/Http/Controllers/WorksheetController.php
+++ app/Http/Controllers/WorksheetController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\worksheet;
 use Illuminate\Http\Request;
 
 class WorksheetController extends Controller
 {
-    //
+    public function index(){
+
+        $data['worksheets']=worksheet::paginate(5);
+        return view('worksheets.index',$data);
+    }
+
+    public function create(){
+
+        return view('worksheets.create');
+    }
+
+    public function edit($id){
+
+        $worksheet= Worksheet::findOrFail($id);
+        return view('worksheets.edit', compact('worksheet'));
+    }
+
+    public function store(Request $request){
+
+        $worksheetData=request()->except('_token');
+        worksheet::insert($worksheetData);
+        return redirect('worksheets');
+    }
+
+    public function destroy($id){
+
+        Enterprise::destroy($id);
+        return redirect('worksheets');
+    }
+
+    public function update(Request $request, $id){
+
+        $worksheetData=request()->except(['_token','_method']);
+        Worksheet::where('id','=', $id)->update($worksheetData);
+        //$worksheet= Worksheets::findOrFail($id);
+        return redirect('worksheets');
+    }
 }
