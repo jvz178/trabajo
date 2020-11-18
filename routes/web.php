9b19eed8c2c10e26dc2eb650bd0a4e939cac8720
SaraCantero <cantero.mesar20@cadiz.salesianos.edu>
2020-11-18 09:36:25 +0100

    sara

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,10 @@ Route::get('/', function () {
 Route::group(['middleware' => 'admin'], function () {
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('users', 'UserController');
-   });
-
 Route::resource('cycles', 'CycleController');
 Route::resource('enterprises', 'EnterpriseController');
 
 
-Auth::routes();
 
 Route::resource('worksheets', 'WorksheetController');
 Route::resource('assistances', 'AssistanceController');
@@ -35,9 +32,5 @@ Route::resource('menuTutorE', 'menuTutorEController');
 Route::resource('menuTutorEn', 'menuTutorEnController');
 
 
-Route::resource('tasks', 'TaskController');
-Route::resource('modules', 'ModuleController');
 
-Route::resource('cce', 'CeController');
-Route::resource('rra', 'RaController');
 
